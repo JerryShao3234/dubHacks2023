076c83456e7ef11961dbac008f3255ea64c8e922
JerryShao3234 <shaojerry@icloud.com>
2023-10-14 22:11:03 -0700

    add labels for every country

--- src/globe/globe.js
+++ src/globe/globe.js
@@ -2,17 +2,10 @@ import React, { useEffect } from 'react';
 import Globe from 'react-globe.gl';
 import ReactGlobe from 'react-globe.gl';
 import { defaultBarMarkerOptions, defaultDotMarkerOptions } from 'react-globe.gl';
-
+import { MAPPED_COUNTRIES } from './../countries.js'
 function SimpleGlobe({country, setCountry}) {
-
+    console.log(MAPPED_COUNTRIES)
     //create a list of labels:
-    var gData = [{
-        lat: 37.7749,
-        lng: -122.4194,
-        color: 'red',
-        text: 'San Francisco',
-        id: '1',
-    }];
 
     var globeEl =  React.useRef();
 
@@ -29,7 +22,7 @@ function SimpleGlobe({country, setCountry}) {
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-blue-marble.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-        labelsData={gData}
+        labelsData={MAPPED_COUNTRIES}
         labelSize={1.3}
         labelDotRadius={0.5}
         onLabelClick={() => {console.log("clicked")} }
