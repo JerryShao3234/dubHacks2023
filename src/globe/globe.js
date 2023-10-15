ee34bdd367b9b22d5a6d8e8356466e3adb4d1350
JerryShao3234 <shaojerry@icloud.com>
2023-10-14 17:29:23 -0700

    Add city labels

--- src/globe/globe.js
+++ src/globe/globe.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import Globe from 'react-globe.gl';
+import ReactGlobe from 'react-globe.gl';
+import { defaultBarMarkerOptions, defaultDotMarkerOptions } from 'react-globe.gl';
 
 function SimpleGlobe() {
 
-    const gData = [...Array(10).keys()].map(() => ({
-        lat: (Math.random() - 0.5) * 180,
-        lng: (Math.random() - 0.5) * 360,
-        size: Math.random() / 3,
-        color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
-    }));
+    //create a list of labels:
+    var gData = [{
+        lat: 37.7749,
+        lng: -122.4194,
+        color: 'red',
+        text: 'San Francisco',
+        id: '1',
+    }];
 
-    return <Globe
+
+    return <ReactGlobe
         globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
         bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
         backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-        pointsData={gData}
-        pointLat="lat"
-        pointLng="lng"
-        pointAltitude="size"
-        pointColor="color"
+        labelsData={gData}
+        labelSize={1.3}
+        labelDotRadius={0.5}
+        onLabelClick={() => {console.log("clicked")} }
+        options={{
+            enableMarkerGlow: true,
+            markerRadiusScaleRange: [1, 2],
+            markerType: 'dot',
+          }}
     />
 }
 
